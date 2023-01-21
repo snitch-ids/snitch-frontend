114b5f004688824a11e80537d7907e1e9b283d49
Marius Kriegerowski <marius.kriegerowski@gmail.com>
2023-01-21 02:15:13 +0100

    login styling

--- src/components/login.rs
+++ src/components/login.rs
@@ -46,14 +46,14 @@ pub fn Login(props: &Props) -> Html {
         <div class="card-base">
         // <form>  // form tag does not work
           <div class="mb-6">
-            <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{"Username"}</label>
-            <input type="text" onchange={onchange} id="username" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="username" required=true />
+            <label for="username" class="input-label">{"Username"}</label>
+            <input type="text" onchange={onchange} id="username" class="input" placeholder="username" required=true />
           </div>
           <div class="mb-6">
-            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{"Your password"}</label>
-            <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=true />
+            <label for="password" class="input-label">{"Your password"}</label>
+            <input type="password" id="password" class="input" required=true />
           </div>
-          <button type="submit" onclick={submit} class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{"Submit"}</button>
+          <button type="submit" onclick={submit} class="button">{"Submit"}</button>
         // <form>
         </div>
     }
