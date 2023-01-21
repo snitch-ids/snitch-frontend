e74119633c850c28470721b0852499fc80602157
Marius Kriegerowski <marius.kriegerowski@gmail.com>
2023-01-21 01:59:23 +0100

    login style

--- src/components/login.rs
+++ src/components/login.rs
@@ -44,16 +44,17 @@ pub fn Login(props: &Props) -> Html {
 
     html! {
         <div class="card-base">
-        <div>
-            <h5 class="card-title">{"Login"}</h5>
+        // <form>  // form tag does not work
+          <div class="mb-6">
+            <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{"Username"}</label>
+            <input type="text" onchange={onchange} id="username" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="username" required=true />
+          </div>
+          <div class="mb-6">
+            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{"Your password"}</label>
+            <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=true />
+          </div>
+          <button type="submit" onclick={submit} class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{"Submit"}</button>
+        // <form>
         </div>
-        <div> {"username"}
-                <input type="text" onchange={onchange}/>
-            </div>
-            <button class="btn" onclick={submit}>
-                { "login" }
-            </button>
-        </div>
-
     }
 }
